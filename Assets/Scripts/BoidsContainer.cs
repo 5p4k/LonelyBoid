e151e4f4319f4c34680b95555320adfb6f52a2a3
Pietro Saccardi <lizardm4@gmail.com>
2024-02-24 20:14:19 +0100

    Remove previous vector field and make instead flow field

--- Assets/Scripts/BoidsContainer.cs
+++ Assets/Scripts/BoidsContainer.cs
@@ -1,27 +1,27 @@
 using System;
 using System.Collections.Generic;
+using Unity.Burst;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UIElements;
 
 public class BoidsContainer : MonoBehaviour
 {
     [Header("Shaders")] public ComputeShader updateShader;
-    public ComputeShader boidsFieldShader;
-    public ComputeShader forceFieldShader;
+    public ComputeShader boidsFlowFieldShader;
 
     private static readonly int TimeID = Shader.PropertyToID("time");
     private static readonly int DeltaTimeID = Shader.PropertyToID("delta_time");
     private static readonly int ForceIndexID = Shader.PropertyToID("force_index");
     private static readonly int FlockIndexID = Shader.PropertyToID("flock_index");
-    private static readonly int TextureWindowID = Shader.PropertyToID("texture_window");
-    private static readonly int TextureSizeID = Shader.PropertyToID("texture_size");
-    private static readonly int TextureOutputID = Shader.PropertyToID("texture_output");
     private static readonly int ForceDataID = Shader.PropertyToID("force_data");
     private static readonly int ForceCountID = Shader.PropertyToID("force_count");
     private static readonly int FlockDataID = Shader.PropertyToID("flock_data");
     private static readonly int FlockCountID = Shader.PropertyToID("flock_count");
     private static readonly int BoidDataID = Shader.PropertyToID("boid_data");
     private static readonly int BoidCountID = Shader.PropertyToID("boid_count");
+    private static readonly int StrideID = Shader.PropertyToID("stride");
+    private static readonly int OrbitsID = Shader.PropertyToID("orbits");
 
     private readonly DualBuffer<BoidData> _boidBuffer = new();
 
@@ -30,6 +30,16 @@ public class BoidsContainer : MonoBehaviour
 
     private Force[] _forces;
     private readonly DualBuffer<ForceData> _forceBuffer = new();
+
+    private readonly DualBuffer<Vector2> _orbitsBuffer = new();
+
+    [Header("Visualization")] public uint orbitLength = 10;
+    public float orbitDensity = 10.0f;
+    public float orbitTimeStep = 0.1f;
+    public bool liveUpdate = true;
+
+    private uint _orbitCount;
+    private static Rect _lastEditorScreen = Rect.zero;
 
     private void PopulateFlocksBuffer(out uint boidsAllocSize)
     {
@@ -72,6 +82,26 @@ public class BoidsContainer : MonoBehaviour
         {
             _forceBuffer.Data[forceIndex++] = force.ToBufferData();
         }
+    }
+
+    private void PopulateOrbitsBuffer(Rect window, out uint orbitsCount)
+    {
+        _orbitsBuffer.Ensure((uint)(orbitDensity * orbitDensity * orbitLength));
+
+        var delta = new Vector2(window.width / orbitDensity, window.height / orbitDensity);
+        var origin = window.min + delta * 0.5f;
+
+        uint orbitIndex = 0;
+        for (uint x = 0; x < orbitDensity; ++x)
+        {
+            for (uint y = 0; y < orbitDensity; ++y)
+            {
+                var position = origin + new Vector2(x, y) * delta;
+                _orbitsBuffer.Data[orbitIndex++ * orbitLength] = position;
+            }
+        }
+
+        orbitsCount = orbitIndex;
     }
 
     private void UpdateBoids(uint boidsCount)
@@ -126,26 +156,22 @@ public class BoidsContainer : MonoBehaviour
                 "Assets/Scripts/BoidsUpdate.compute", typeof(ComputeShader));
         }
 
-        if (boidsFieldShader == null)
+        if (boidsFlowFieldShader == null)
         {
-            boidsFieldShader = (ComputeShader)AssetDatabase.LoadAssetAtPath(
-                "Assets/Scripts/BoidsField.compute", typeof(ComputeShader));
+            boidsFlowFieldShader = (ComputeShader)AssetDatabase.LoadAssetAtPath(
+                "Assets/Scripts/BoidsFlowField.compute", typeof(ComputeShader));
         }
+    }
 
-        if (forceFieldShader == null)
-        {
-            forceFieldShader = (ComputeShader)AssetDatabase.LoadAssetAtPath(
-                "Assets/Scripts/ForceField.compute", typeof(ComputeShader));
-        }
+    private void OnValidate()
+    {
+        BoidsContainerEditor.EditorScreenWindowChanged(out var window);
+        RecomputeFlowField(window);
     }
 #endif
 
-    private void ComputeBoidsUpdate()
+    private void ComputeBoidsUpdate(uint boidsCount)
     {
-        PopulateFlocksBuffer(out var boidsAllocSize);
-        PopulateBoidsBuffer(boidsAllocSize, out var boidsCount);
-        PopulateForcesBuffer();
-
         _flockBuffer.Bind(updateShader, 0, FlockDataID, FlockCountID);
         _boidBuffer.Bind(updateShader, 0, BoidDataID, boidsCount, BoidCountID);
         _forceBuffer.Bind(updateShader, 0, ForceDataID, ForceCountID);
@@ -158,10 +184,31 @@ public class BoidsContainer : MonoBehaviour
         _boidBuffer.ToLocal();
         UpdateBoids(boidsCount);
     }
+    
+    private void ComputeFlowField(uint flockIndex, uint boidsCount, uint orbitsCount)
+    {
+        _flockBuffer.Bind(boidsFlowFieldShader, 0, FlockDataID, FlockCountID);
+        _boidBuffer.Bind(boidsFlowFieldShader, 0, BoidDataID, boidsCount, BoidCountID);
+        _forceBuffer.Bind(boidsFlowFieldShader, 0, ForceDataID, ForceCountID);
+        _orbitsBuffer.Bind(boidsFlowFieldShader, 0, OrbitsID);
+
+        boidsFlowFieldShader.SetInt(FlockIndexID, (int)flockIndex);
+        boidsFlowFieldShader.SetFloat(TimeID, Time.time);
+        boidsFlowFieldShader.SetFloat(DeltaTimeID, orbitTimeStep);
+        boidsFlowFieldShader.SetInt(StrideID, (int)orbitLength);
+
+        boidsFlowFieldShader.Dispatch(0, (int)orbitsCount, 1, 1);
+
+        _orbitsBuffer.ToLocal();
+    }
 
     private void Update()
     {
-        ComputeBoidsUpdate();
+        PopulateFlocksBuffer(out var boidsAllocSize);
+        PopulateBoidsBuffer(boidsAllocSize, out var boidsCount);
+        PopulateForcesBuffer();
+
+        ComputeBoidsUpdate(boidsCount);
 
         // Post update actions
         foreach (var flock in _flocks)
@@ -169,104 +216,53 @@ public class BoidsContainer : MonoBehaviour
             flock.KillStrayBoids();
             flock.SpawnIfNeeded();
         }
+
+#if UNITY_EDITOR
+        if (!liveUpdate) return;
+
+        BoidsContainerEditor.EditorScreenWindowChanged(out var window);
+        PopulateOrbitsBuffer(window, out _orbitCount);
+        ComputeFlowField(0, boidsCount, _orbitCount);
+#endif
     }
-
-    private static void BindTexture(ComputeShader shader, Texture texture, Rect window)
+    
+    public void RecomputeFlowField(Rect window)
     {
-        
-        var texWin = new[] { window.xMin, window.yMin, window.width, window.height };
-        var texSz = new[] { texture.width, texture.height };
-
-        shader.SetFloats(TextureWindowID, texWin);
-        shader.SetInts(TextureSizeID, texSz);
-        shader.SetTexture(0, TextureOutputID, texture);
-    }
-
-    public void ComputeField(Force force, Texture texture, Rect window)
-    {
-        var forceIndex = Array.IndexOf(_forces, force);
-        if (forceIndex < 0) return;
-
-        PopulateForcesBuffer();
-
-        _forceBuffer.Bind(forceFieldShader, 0, ForceDataID);
-        BindTexture(forceFieldShader, texture, window);
-
-        forceFieldShader.SetInt(ForceIndexID, forceIndex);
-        forceFieldShader.SetFloat(TimeID, Time.time);
-
-        forceFieldShader.Dispatch(0, texture.width, texture.height, 1);
-    }
-
-    public void ComputeField(Flock flock, Texture texture, Rect window)
-    {
-        var flockIndex = Array.IndexOf(_flocks, flock);
-        if (flockIndex < 0) return;
+        if (_flocks == null || _flocks.Length == 0)
+        {
+            // TODO not this
+            Reload();
+        }
         
         PopulateFlocksBuffer(out var boidsAllocSize);
         PopulateBoidsBuffer(boidsAllocSize, out var boidsCount);
         PopulateForcesBuffer();
+        PopulateOrbitsBuffer(window, out _orbitCount);
 
-        _flockBuffer.Bind(boidsFieldShader, 0, FlockDataID, FlockCountID);
-        _boidBuffer.Bind(boidsFieldShader, 0, BoidDataID, boidsCount, BoidCountID);
-        _forceBuffer.Bind(boidsFieldShader, 0, ForceDataID, ForceCountID);
-        BindTexture(boidsFieldShader, texture, window);
-
-        boidsFieldShader.SetInt(FlockIndexID, flockIndex);
-        boidsFieldShader.SetFloat(TimeID, Time.time);
-
-        boidsFieldShader.Dispatch(0, texture.width, texture.height, 1);
+        ComputeFlowField(0, boidsCount, _orbitCount);
     }
 
-    public void ComputeAccelerationField(Flock flock, Rect window, RenderTexture texture)
+#if UNITY_EDITOR
+    public void DrawFlowFieldHandles()
     {
-        // bool didReload = false;
-        // if (_flockData == null)
-        // {
-        //     didReload = true;
-        //     Recache();
-        // }
-        //
-        // uint boidCount = 0;
-        // int flockIndex = GeometryToBuffers(out boidCount, flock);
-        //
-        // if (flockIndex < 0 && !didReload)
-        // {
-        //     didReload = true;
-        //     Recache();
-        //     // Retry
-        //     flockIndex = GeometryToBuffers(out boidCount, flock);
-        // }
-        //
-        // if (flockIndex < 0)
-        // {
-        //     Debug.LogError("Flock not found in this manager.");
-        //     return;
-        // }
-        //
-        // _boidDataBuffer.SetData(_boidData);
-        // _flockDataBuffer.SetData(_flockData);
-        // _forceDataBuffer.SetData(_forceData);
-        //
-        // boidsFieldShader.SetBuffer(0, "boidData", _boidDataBuffer);
-        // boidsFieldShader.SetBuffer(0, "flockData", _flockDataBuffer);
-        // boidsFieldShader.SetBuffer(0, "forceData", _forceDataBuffer);
-        //
-        // boidsFieldShader.SetInt("boidCount", (int)boidCount);
-        // boidsFieldShader.SetInt("flockIndex", flockIndex);
-        // boidsFieldShader.SetInt("forceCount", (int)_forceData.Length);
-        //
-        // boidsFieldShader.SetFloat("time", Time.time);
-        //
-        // float[] texWin = new float[4] { window.xMin, window.yMin, window.width, window.height };
-        // int[] texSz = new int[2] { texture.width, texture.height };
-        //
-        // boidsFieldShader.SetFloats("textureWindow", texWin);
-        // boidsFieldShader.SetInts("textureSize", texSz);
-        //
-        // boidsFieldShader.SetTexture(0, "textureOutput", texture);
-        // boidsFieldShader.Dispatch(0, texture.width, texture.height, 1);
+        Vector2 pxSize = Camera.current.ScreenToWorldPoint(new Vector3(1, 1, 0))
+                         - Camera.current.ScreenToWorldPoint(Vector3.zero);
+
+        var discRadius = 2.0f * Mathf.Max(pxSize.x, pxSize.y);
+
+        for (uint i = 0; i < _orbitCount * orbitLength; ++i)
+        {
+            if (i % orbitLength == 0)
+            {
+                Handles.DrawSolidDisc(_orbitsBuffer.Data[i], Vector3.back, discRadius);
+            }
+            else
+            {
+                Handles.DrawLine(_orbitsBuffer.Data[i - 1], _orbitsBuffer.Data[i]);
+            }
+        }
     }
+#endif
 
 #if UNITY_EDITOR
     [CustomEditor(typeof(BoidsContainer))]
@@ -306,6 +302,41 @@ public class BoidsContainer : MonoBehaviour
 
             EditorGUILayout.EndHorizontal();
         }
+
+        public static bool EditorScreenWindowChanged(out Rect window)
+        {
+            if (!Camera.current)
+            {
+                window = Rect.zero;
+                return false;
+            }
+
+            var llc = Camera.current.ViewportToWorldPoint(new Vector3(0f, 0f, 0));
+            var urc = Camera.current.ViewportToWorldPoint(new Vector3(1f, 1f, 0));
+
+            window = new Rect(llc, urc - llc);
+
+            var changed = window == _lastEditorScreen;
+            _lastEditorScreen = window;
+
+            return changed;
+        }
+
+        public void OnSceneGUI()
+        {
+            var container = target as BoidsContainer;
+
+            if (!Application.isPlaying)
+            {
+                if (EditorScreenWindowChanged(out var window))
+                {
+                    _lastEditorScreen = window;
+                    container!.RecomputeFlowField(window);
+                }
+            }
+
+            container!.DrawFlowFieldHandles();
+        }
     }
 #endif
 }
@@ -319,7 +350,7 @@ public class DualBuffer<T>
 
     private ComputeBuffer _computeBuffer;
 
-    private uint AllocSize => (uint)(Data?.Length ?? 0);
+    public uint AllocSize => (uint)(Data?.Length ?? 0);
 
     public void Ensure(uint allocSize)
     {
--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -64,16 +64,7 @@ public class Flock : MonoBehaviour
     [HideInInspector]
     public SortedSet<Boid> boids = new SortedSet<Boid>(new BoidComparer());
 
-    [Header("Acceleration Field")]
-    public int resolution = 16;
-    public bool liveUpdateWhenPlaying = false;
-    public float liveUpdateMaxFps = 15;
-
     float _lastSpawn = 0.0f;
-    float _lastFieldUpdate = 0.0f;
-
-    RenderTexture _accelFieldRT;
-    Texture2D _accelFieldLocal;
 
     ObjectPool<Boid> _boidsPool;
 
@@ -190,13 +181,6 @@ public class Flock : MonoBehaviour
         );
     }
 
-    void Update() {
-        float fieldRefreshPeriod = (liveUpdateMaxFps > 0.0f) ? 1.0f / liveUpdateMaxFps : 0.0f;
-        if (liveUpdateWhenPlaying && Time.time - _lastFieldUpdate > fieldRefreshPeriod) {
-            RefreshAccelerationField();
-        }
-    }
-
     public Rect accelerationFieldDomain {
         get {
             float radius = Mathf.Max(spawnRadius, killRadius);
@@ -207,53 +191,7 @@ public class Flock : MonoBehaviour
         }
     }
 
-    public Texture2D accelerationField {
-        get {
-            if (_accelFieldRT != null) {
-                return _accelFieldLocal;
-            }
-            return null;
-        }
-    }
-
-    public void RefreshAccelerationField() {
-        if (_accelFieldRT == null || _accelFieldRT.width != resolution) {
-            ClearAccelerationField();
-            _accelFieldRT = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.RGFloat, RenderTextureReadWrite.Linear);
-            _accelFieldRT.enableRandomWrite = true;
-            _accelFieldRT.Create();
-        }
-
-        GetContainer().ComputeAccelerationField(this, accelerationFieldDomain, _accelFieldRT);
-
-        if (_accelFieldLocal == null || _accelFieldLocal.width != resolution)
-        {
-            _accelFieldLocal = new Texture2D(_accelFieldRT.width, _accelFieldRT.height, TextureFormat.RGFloat, false);
-        }
-
-        // Copy to the cache because we cannot directly access a rendertexture content:
-        // RenderTextures do not exist on the CPU side.
-
-        // When rendering gizmos, we're actually rendering to a texture. Gotta save that
-        var oldRenderTexture = RenderTexture.active;
-        // Lifesaver: https://discussions.unity.com/t/convert-a-rendertexture-to-a-texture2d/946/2
-        RenderTexture.active = _accelFieldRT;
-        _accelFieldLocal.ReadPixels(new Rect(0, 0, _accelFieldRT.width, _accelFieldRT.height), 0, 0);
-        _accelFieldLocal.Apply();
-        RenderTexture.active = oldRenderTexture;
-
-        _lastFieldUpdate = Time.time;
-    }
-
-    public void ClearAccelerationField() {
-        if (_accelFieldRT != null) {
-            _accelFieldRT.Release();
-            _accelFieldRT = null;
-        }
-    }
-
     void OnDestroy() {
-        ClearAccelerationField();
         _boidsPool.Dispose();
     }
 }
@@ -263,46 +201,9 @@ public class Flock : MonoBehaviour
 [CustomEditor(typeof(Flock))]
 public class FlockEditor : Editor {
 
-    static void DrawVectorField(Rect rect, Texture2D field, float baseScaleFactor) {
-        Vector2Int size = new Vector2Int(field.width, field.height);
-        Vector2 pixelSize  = new Vector2(rect.width / size.x, rect.height / size.y);
-
-        float cellSize = Mathf.Min(pixelSize.x, pixelSize.y);
-
-        var data = field.GetRawTextureData<Vector2>();
-        int i = 0;
-        for (int y = 0; y < size.y; ++y) {
-            for (int x = 0; x < size.x; ++x) {
-                Vector2 pos = rect.min + 0.5f * pixelSize + pixelSize * new Vector2(x, y);
-                Vector2 v = data[i++] * baseScaleFactor;
-
-                // Clamp length at cell size, but make thicker from there onwards
-                float length = v.magnitude;
-                float scale = Mathf.Max(0.0f, length / cellSize);
-                
-                if (length > cellSize) {
-                    v = v.normalized * cellSize;
-                }
-
-                Handles.DrawSolidDisc(pos, Vector3.back, 0.05f * cellSize * scale);
-                Handles.DrawLine(pos, pos + v, scale);
-            }
-        }
-    }
-
     [DrawGizmo(GizmoType.InSelectionHierarchy | GizmoType.NotInSelectionHierarchy)]
     static void DrawGizmo(Flock flock, GizmoType gizmoType) {
         bool active = (gizmoType & GizmoType.Active) != 0;
-
-        if (flock.accelerationField != null) {
-            DrawVectorField(flock.accelerationFieldDomain, flock.accelerationField, 1.0f / flock.maxAcceleration);
-        } else {
-            for (uint i = 0; i < 36; ++i) {
-                Vector3 dir = Quaternion.AngleAxis(i * 10.0f, Vector3.back) * Vector3.up;
-                Handles.DrawLine(flock.transform.position + flock.spawnRadius * dir,
-                                 flock.transform.position + flock.killRadius * dir);
-            }
-        }
 
         using (new Handles.DrawingScope(active ? Color.green : Handles.color)) {
             Handles.DrawWireDisc(flock.transform.position, Vector3.back, flock.spawnRadius);
@@ -331,26 +232,6 @@ public class FlockEditor : Editor {
         using (new Handles.DrawingScope(Color.red)) {
             HandleRadius(flock, ref flock.killRadius, "Change kill radius");
         }
-    }
-
-    public override void OnInspectorGUI() {
-        Flock flock = target as Flock;
-
-        DrawDefaultInspector();
-
-        EditorGUILayout.BeginHorizontal();
-
-        if (GUILayout.Button("Refresh field")) {
-            flock.RefreshAccelerationField();
-            UnityEditor.SceneView.RepaintAll();
-        }
-
-        if (GUILayout.Button("Clear field")) {
-            flock.ClearAccelerationField();
-            UnityEditor.SceneView.RepaintAll();
-        }
-
-        EditorGUILayout.EndHorizontal();
     }
 }
 #endif
