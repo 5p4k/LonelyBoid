c989e4db7bd6600a3dfe9b11e78cc6f808e92810
Pietro Saccardi <lizardm4@gmail.com>
2024-03-19 21:40:28 +0100

    Implement force (as it was before)

--- Packages/LonelyBoid/Runtime/Force.cs
+++ Packages/LonelyBoid/Runtime/Force.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
@@ -8,11 +5,68 @@ namespace saccardi.lonelyboid
 {
     namespace IO
     {
+        [SuppressMessage("ReSharper", "NotAccessedField.Global")]
+        internal struct TurbulentForce
+        {
+            public float spatialScale;
+            public float temporalScale;
+        };
+
+        [SuppressMessage("ReSharper", "NotAccessedField.Global")]
+        internal struct RadialForce
+        {
+            public float falloffPower;
+        };
+
+
+        [SuppressMessage("ReSharper", "NotAccessedField.Global")]
         internal struct ForceData
         {
+            public Vector2 origin;
+            public int type;
+            public float intensity;
+
+            public TurbulentForce turbulent;
+            public RadialForce radial;
+
+            public static ForceData From(Force force)
+            {
+                return new ForceData
+                {
+                    origin = force.transform.position,
+                    type = (int)force.type,
+                    intensity = force.intensity,
+                    turbulent = new TurbulentForce
+                    {
+                        spatialScale = force.spatialScale,
+                        temporalScale = force.temporalScale
+                    },
+                    radial = new RadialForce
+                    {
+                        falloffPower = force.falloffPower
+                    }
+                };
+            }
         }
     }
+
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public enum ForceType
+    {
+        Radial = 1,
+        Turbulence = 2
+    }
+
     public class Force : MonoBehaviour
     {
+        [Header("Physics")] public ForceType type = ForceType.Radial;
+        public float intensity = 1.0f;
+
+        // Radial field only:
+        public float falloffPower = 2.0f;
+
+        // Turbulence field only
+        public float spatialScale = 0.5f;
+        public float temporalScale = 1.0f;
     }
 }
