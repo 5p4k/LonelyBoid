21268bd80f528c689abd9105116e72b62b09894f
Pietro Saccardi <lizardm4@gmail.com>
2024-03-29 21:05:35 +0100

    Fix kill on collide

--- Assets/Scripts/KillOnCollide.cs
+++ Assets/Scripts/KillOnCollide.cs
@@ -5,7 +5,8 @@ public class KillOnCollide : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.TryGetComponent<Boid>(out var boid))
+        if (!other.TryGetComponent<Boid>(out var boid)) return;
+        if (boid.flock)
         {
             boid.flock.Kill(boid);
         }
